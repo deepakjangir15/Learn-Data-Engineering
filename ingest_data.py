1f3cba9dbfe6a30cb7ba4f7266f10b0a23d47c70
Deepak Jangir <tdeepak509@gmail.com>
2023-06-15 20:16:36 +0100

    Initialized basic Prefect flow

--- ingest_data.py
+++ ingest_data.py
@@ -6,11 +6,12 @@ from time import time
 import pandas as pd
 from sqlalchemy import create_engine
 from prefect import flow, task
+from prefect.tasks import task_input_hash
+from datetime import timedelta
 
 
-@task(log_prints=True, retries=3)
-def ingest_data(user, password, host, port, db, table_name, url):
-
+@task(log_prints=True, retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
+def extract_data(url):
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
     if url.endswith('.csv.gz'):
@@ -19,8 +20,6 @@ def ingest_data(user, password, host, port, db, table_name, url):
         csv_name = 'output.csv'
 
     os.system(f"wget {url} -O {csv_name}")
-    postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
-    engine = create_engine(postgres_url)
 
     df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
@@ -28,23 +27,42 @@ def ingest_data(user, password, host, port, db, table_name, url):
 
     df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
     df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    return df
+
+
+@task(log_prints=True)
+def transform_data(df):
+    print(
+        f"Pre: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    df = df[df['passenger_count'] != 0]
+
+    print(
+        f"Post: missing passenger count: {df['passenger_count'].isin([0]).sum()}")
+    return df
+
+
+@task(log_prints=True, retries=3)
+def ingest_data(user, password, host, port, db, table_name, df):
+    postgres_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+    engine = create_engine(postgres_url)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
-
     df.to_sql(name=table_name, con=engine, if_exists='append')
 
 
 @flow(name='Ingest Flow')
 def main_flow():
-    user = "postgres"
-    password = "admin"
+    user = "root"
+    password = "root"
     host = "localhost"
-    port = "5433"
+    port = "5432"
     db = "ny_taxi"
     table_name = "yellow_taxi_trips"
     csv_url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
 
-    ingest_data(user, password, host, port, db, table_name, csv_url)
+    raw_data = extract_data(csv_url)
+    data = transform_data(raw_data)
+    ingest_data(user, password, host, port, db, table_name, data)
 
 
 if __name__ == '__main__':
